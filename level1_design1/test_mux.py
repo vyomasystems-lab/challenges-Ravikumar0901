da8a0c0169eb0837cda461cf63aa1a57d04b6c19
Ravikumar0901 <nanuboluravikumar@gmail.com>
2022-07-25 20:47:12 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,11 +9,12 @@ async def test_mux(dut):
 
     cocotb.log.info('##### CTB: Develop your test here ########')
     """Test for input as 1101 """
-    S=1101
+    S=1100
     #input Driving
     dut.sel.value = S
 
     await Timer(2,units='2')
-    assert dut.out.value == inp13,f"Mux Result is incorrect: {dut.X.value}!=13"
+    assert dut.out.value == inp12,f"Mux Result is incorrect: {dut.X.value}!=12"
+
 
     
