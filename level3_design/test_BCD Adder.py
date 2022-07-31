4fd514557d68d831b5de2f0af7478385b038228c
Ravikumar0901 <nanuboluravikumar@gmail.com>
2022-07-31 14:03:35 +0000

    update the files

---
+++ level3_design/test_BCD Adder.py
@@ -0,0 +1,25 @@
+# See LICENSE.cocotb for details
+# See LICENSE.vyoma for details
+
+# Simple tests for an adder module
+import cocotb
+from cocotb.triggers import Timer
+import random
+
+@cocotb.test()
+async def adder_basic_test(dut):
+    """Test for 5 + 3"""
+
+    A = 5
+    B = 3
+    Cin=0
+
+    # input driving
+    dut.a.value = A
+    dut.b.value = B
+    dut.cin.value = Cin
+
+    await Timer(2, units='ns')
+
+    assert dut.sum.value == A+B, f"Adder result is incorrect: {dut.X.value} != 8"
+
